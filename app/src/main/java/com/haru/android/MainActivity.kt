90e9a17c657215ea5b049c5f069cea27e4e6e0e3
Dahoonkk <iamkdh1@naver.com>
2023-07-27 22:24:54 +0900

    test

--- app/src/main/java/com/haru/android/MainActivity.kt
+++ app/src/main/java/com/haru/android/MainActivity.kt
@@ -2,6 +2,7 @@ package com.haru.android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.haru.android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +13,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        Log.e("test", "test")
     }
 }
